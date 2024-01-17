2cb65965145f4b83f51c5529ebdb183df3600e15
javeria-farooq <gettingeducation6@gmail.com>
2024-01-17 10:01:28 +0500

    10th JS Assignment

---
+++ app.js
@@ -0,0 +1,40 @@
+// let name = +prompt("Your Name:")
+// let urdu = +prompt("Your obtained marks in Urdu:")
+// let eng = +prompt("Your obtained marks in English:")
+// let math = +prompt("Your obtained marks in Maths:")
+// let comp = +prompt("Your obtained marks in Computer:")
+// let islam = +prompt("Your obtained marks in Islamiat:")
+// let total_obtMarks = urdu + eng + math + comp  + islam
+// alert(total_obtMarks)
+// let percent = 100 * (total_obtMarks / 500)
+
+// if(percent >= 80 && percent < 101) {
+//     alert("A+")
+// }
+
+// else if(percent >=70 && percent < 80) {
+//     alert("A")
+// }
+
+// else if(percent >= 60 && percent < 70) {
+//     alert("B")
+// }
+
+// else if(percent >= 50 && percent < 60) {
+//     alert("C")
+// }
+
+// else if(percent >= 40 && percent < 50) {
+//     alert("C")
+// }
+
+// else if(percent > 100) {
+//     alert("invalid percentage")
+// }
+
+// else{
+//     alert("Fail (F)")
+// }
+
+// let per_show = percent + "%"
+// alert(per_show)
